8241cc9b45e1c8b3fd1f23f3388cef6b9536dd9e
Rousgidraph <rousgidraph@gmail.com>
2021-12-27 17:01:31 +0300

    ui done

---
+++ colourful-design/js/main.js
@@ -0,0 +1,13 @@
+
+
+function dark_mode(){
+    window.alert("Changing to dark mode...")
+}
+
+function to_more_details(){
+    window.alert("Learning more ")
+}
+
+function to_leave_review(){
+    window.alert("Reviewing ")
+}
