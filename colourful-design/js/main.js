9a7074e5a858aec144fea3bf4193521b08ef3ea2
Rousgidraph <rousgidraph@gmail.com>
2021-12-27 17:37:12 +0300

    dark mode achieved 😎

--- colourful-design/js/main.js
+++ colourful-design/js/main.js
@@ -1,7 +1,20 @@
 
-
+var dark = false;
+var sun = "assets/sun-icon.png"
+var moon = "assets/moon-icon.png";
 function dark_mode(){
-    window.alert("Changing to dark mode...")
+    //window.alert("Changing to dark mode...")
+    var cont = document.getElementById('container')
+    var btn = document.getElementById('dark-mode-toggle')
+    if(dark){
+    cont.className = "container day"
+    btn.src = moon
+    }else{
+    cont.className = "container dark"
+    btn.src=sun
+    }
+    dark = !dark
+    console.log("somthing here")
 }
 
 function to_more_details(){
